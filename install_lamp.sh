113b98efafc3ddfa0a20219482ac288811ba158c
alanhuang281 <109663578@qq.com>
2018-09-09 12:51:46 +0800

    add

---
+++ install_lamp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+until apt-get -y update
+do
+  echo "Try again"
+  sleep 5
+done
+
+# set up a silent install of MySQL
+dbpass=complish
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# install the LAMP stack
+until apt-get -y install apache2 mysql-server php5 php5-mysql
+do
+  echo "Try again"
+  sleep 5
+done
+# write some PHP
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
+
+# restart Apache
+apachectl restart
